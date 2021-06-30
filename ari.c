129cb92499b549369f234299561977dffc286d73
annm-github <71374618+annm-github@users.noreply.github.com>
2021-06-30 18:42:25 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -27,7 +27,7 @@ for(int j=0;j<100;j++)
              {
              word++;
              }
-           else (str[i] =='.')
+           if(str[i] =='.')
                  {  sent++;
                  }
     }
