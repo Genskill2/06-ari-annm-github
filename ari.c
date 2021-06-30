57aca3cf942351b6f1c97b8c577c559881b1b444
annm-github <71374618+annm-github@users.noreply.github.com>
2021-06-30 18:49:41 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -27,8 +27,7 @@ for(int j=0;j<100;j++)
              {
              word++;
              }
-          else
-               sent++;
+          else(sent++);
                  
     }
     float ar=(4.71*(totChar/word)+0.5*(word/sent) -21.43);
